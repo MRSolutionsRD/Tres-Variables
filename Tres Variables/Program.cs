805bbcb7e56981183513627f2e89832b8eca8417
MRSolutionsRD <61673292+MRSolutionsRD@users.noreply.github.com>
2020-03-06 12:56:54 -0400

    Add files via upload

---
+++ Tres Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Tres_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int va1 = 5;
+            int va2 = 8;
+            int va3 = 2;
+            int sm = va1 + va2 + va3;
+
+            Console.WriteLine(sm);
+                       
+            Console.ReadKey();
+        }
+    }
+}
